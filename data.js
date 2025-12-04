11f28f646ae7e6fdf22685c668d97d713a8367c7
albertoahumada2004-ctrl <albertoahumada2004@gmail.com>
2025-12-04 12:18:59 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,561 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-12",
+      "name": "12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3444352074729764,
+          "pitch": 0.4963999082973505,
+          "rotation": 15.707963267948973,
+          "target": "1-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-11",
+      "name": "11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.31932391389793047,
+          "pitch": 0.32042978917384346,
+          "rotation": 10.995574287564278,
+          "target": "0-12"
+        },
+        {
+          "yaw": 1.784296831321801,
+          "pitch": 0.39820939046410153,
+          "rotation": 9.42477796076938,
+          "target": "3-9"
+        },
+        {
+          "yaw": -2.0591039843414407,
+          "pitch": 0.5923522470517142,
+          "rotation": 0,
+          "target": "2-10"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.02202939639098922,
+          "pitch": -0.16047892935224795,
+          "title": "Quirófano&nbsp; &nbsp; &nbsp;",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.9505251653737066,
+          "pitch": -0.05665845160666905,
+          "title": "Lavamanos&nbsp; &nbsp; &nbsp;",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-10",
+      "name": "10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7011114634300366,
+          "pitch": 0.5999775261492637,
+          "rotation": 4.71238898038469,
+          "target": "1-11"
+        },
+        {
+          "yaw": 3.092318426909393,
+          "pitch": 0.2463553221325565,
+          "rotation": 3.141592653589793,
+          "target": "3-9"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.13310945109686223,
+          "pitch": -0.24728820436775933,
+          "title": "Lavamanos&nbsp; &nbsp;&nbsp;",
+          "text": "<br>"
+        },
+        {
+          "yaw": 2.4613779631906763,
+          "pitch": -0.14575831278928675,
+          "title": "Quirófano&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "3-9",
+      "name": "9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05065393309924815,
+          "pitch": 0.2977541349884554,
+          "rotation": 0,
+          "target": "2-10"
+        },
+        {
+          "yaw": -2.274326236737881,
+          "pitch": 0.4449162207052968,
+          "rotation": 7.853981633974483,
+          "target": "4-8"
+        },
+        {
+          "yaw": 0.2242912672321431,
+          "pitch": 0.3625852668552252,
+          "rotation": 7.853981633974483,
+          "target": "1-11"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05988397802033418,
+          "pitch": -0.07081250260736383,
+          "title": "Lavamanos&nbsp; &nbsp;&nbsp;",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.31652932022463176,
+          "pitch": -0.0902004532371734,
+          "title": "Quirófano&nbsp; &nbsp; &nbsp;",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-8",
+      "name": "8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "yaw": -1.5315853812766633,
+        "pitch": 0.03160112680861005,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10967002797805492,
+          "pitch": 0.5340213842148636,
+          "rotation": 4.71238898038469,
+          "target": "3-9"
+        },
+        {
+          "yaw": 1.715555874243834,
+          "pitch": 0.31239200481789986,
+          "rotation": 3.141592653589793,
+          "target": "5-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8204092289450138,
+          "pitch": -0.12907054299560095,
+          "title": "Quirófano&nbsp; &nbsp; &nbsp; &nbsp;",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.930893447364471,
+          "pitch": -0.0849828821859866,
+          "title": "Área de unidad de cuidados intensivos&nbsp;&nbsp;",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "5-7",
+      "name": "7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4642719514879676,
+          "pitch": 0.35806211024472745,
+          "rotation": 0,
+          "target": "4-8"
+        },
+        {
+          "yaw": -1.686667447947137,
+          "pitch": 0.5438883718438046,
+          "rotation": 0,
+          "target": "8-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.13734184139924466,
+          "pitch": -0.4039747099331059,
+          "title": "Area de unidad de cuidados intensivos&nbsp; &nbsp;",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "6-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9624190486833335,
+          "pitch": 0.07795544815176925,
+          "rotation": 0,
+          "target": "8-2"
+        },
+        {
+          "yaw": -1.0835855742939913,
+          "pitch": 0.03098406195332437,
+          "rotation": 14.137166941154074,
+          "target": "5-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.14952040462050142,
+          "pitch": -0.3703309797148968,
+          "title": "Central de esterilización&nbsp; &nbsp; &nbsp;",
+          "text": "&nbsp;"
+        },
+        {
+          "yaw": 0.5439338238110043,
+          "pitch": -0.34367348472943604,
+          "title": "Oficina de estudio&nbsp; &nbsp;&nbsp;",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "7-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1075456402610584,
+          "pitch": 0.3324725074089514,
+          "rotation": 0,
+          "target": "8-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.601456200780107,
+          "pitch": 0.21164361775123197,
+          "rotation": 3.141592653589793,
+          "target": "9-1"
+        },
+        {
+          "yaw": -2.186375882477652,
+          "pitch": 0.4598801458427282,
+          "rotation": 19.63495408493622,
+          "target": "5-7"
+        },
+        {
+          "yaw": -1.2514129258756768,
+          "pitch": 0.10785565668791364,
+          "rotation": 0,
+          "target": "7-4"
+        },
+        {
+          "yaw": -0.4202304991381798,
+          "pitch": 0.056329998814756976,
+          "rotation": 0,
+          "target": "6-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4516622032731954,
+          "pitch": -0.0716747904458046,
+          "title": "Recepción&nbsp; &nbsp;&nbsp;",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.1180488907249195,
+          "pitch": -0.28300019307893365,
+          "title": "Baños de caballero&nbsp; &nbsp;",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.7686995020983236,
+          "pitch": -0.24535894355772925,
+          "title": "Baños de Dama&nbsp; &nbsp;",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.4544282613255781,
+          "pitch": -0.18924787148049305,
+          "title": "Sala de espera&nbsp; &nbsp;",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "9-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1504,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06920067002200625,
+          "pitch": 0.18547319351805847,
+          "rotation": 0,
+          "target": "8-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.757148593105847,
+          "pitch": -0.26529835346485164,
+          "title": "Recepción&nbsp; &nbsp; &nbsp;&nbsp;",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
